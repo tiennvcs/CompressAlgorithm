a1697b2b6c76402f84a8e5f5fd26a96cfe5c0821
Trần Đình Khang <trandinhkhang0279@gmail.com>
2020-07-20 09:00:05 +0700

    reformat code

--- algorithms/rlc.py
+++ algorithms/rlc.py
@@ -2,19 +2,11 @@ import argparse
 import os
 import time
 
-def get_arguments():
-    parser = argparse.ArgumentParser(description='The Run Length Coding algorithms')
-    parser.add_argument('--mode', '-m', default='compression',
-                        choices=['compression', 'decompression'],
-                        help='The mode for the algorithm work')
-    parser.add_argument('--input', '-i', default='./input/input_rlc.txt',
-                        help='The input file path')
-    parser.add_argument('--output', '-o', default='./output/output_rlc.txt',
-                        help='The output file path')
-    return vars(parser.parse_args())
+
+"""RLC relative function"""
 
 
-def compression_ratio(input_string:str, encoded_string:str):
+def compression_ratio(input_string: str, encoded_string: str):
     b0 = len(input_string)
     b1 = len(encoded_string)
     return b0 / b1
@@ -23,16 +15,15 @@ def compression_ratio(input_string:str, encoded_string:str):
 def rlc_compression(input_string: str):
     print("[INFO] The string need encode is \n\t{}".format(input_string))
     res_string = str()
-
     l = len(input_string)
     i = 0
 
     print("[INFO] Encoding ...")
-    while (i < l - 1):
+    while i < l - 1:
 
-        # Count the occurency of current word
+        # Count the occurrence of current word
         count = 1
-        while (i < l and input_string[i] == input_string[i+1]):
+        while i < l and input_string[i] == input_string[i + 1]:
             count += 1
             i += 1
 
@@ -41,11 +32,10 @@ def rlc_compression(input_string: str):
 
         # Append the current character with number of times it appear
         res_string = res_string + "{}{}".format(input_string[i], count)
-        i+= 1
+        i += 1
 
     print("--> The string {} is encoded with code is {}".format({input_string}, {res_string}))
     return res_string
-
 
 
 def rlc_decompression(encoded_string):
@@ -68,29 +58,44 @@ def rlc_decompression(encoded_string):
     print("[INFO] The decoded string is {}".format(res_string))
     return res_string
 
-def main(args):
-    # Check the input path is exis whether or not
-    if not os.path.isfile(args['input']):
-        print("The input file is not exis")
+
+"""Processing Function"""
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='The Run Length Coding algorithms')
+    parser.add_argument('--mode', '-m', default='compression',
+                        choices=['compression', 'decompression'],
+                        help='The mode for the algorithm work')
+    parser.add_argument('--input', '-i', default='./input/input_rlc.txt',
+                        help='The input file path')
+    parser.add_argument('--output', '-o', default='./output/output_rlc.txt',
+                        help='The output file path')
+    return vars(parser.parse_args())
+
+
+def main(_args):
+    # Check the input path is exist whether or not
+    if not os.path.isfile(_args['input']):
+        print("The input file is not exist")
         exit(1)
     # Read the text data from file
-    with open(args['input'], 'r') as f:
+    with open(_args['input'], 'r') as f:
         string = f.read()
     # Check the mode
-    if args['mode'] == 'compression':
+    if _args['mode'] == 'compression':
         result = rlc_compression(input_string=string)
         compress_ratio = compression_ratio(input_string=string, encoded_string=result)
         print("[INFO] The compression ratio is {}".format(compress_ratio))
         # Store the output data to disk
-        with open(args['output'], 'w') as f:
+        with open(_args['output'], 'w') as f:
             f.write(result)
 
-    elif args['mode'] == 'decompression':
+    elif _args['mode'] == 'decompression':
         rlc_decompression(string)
     else:
         print("The selected mode is not valid")
         exit(0)
-
 
 
 if __name__ == '__main__':
