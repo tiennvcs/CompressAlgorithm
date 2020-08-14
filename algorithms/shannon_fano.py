1ccdbacf06dac9e09d191cddb3d82cb2889a29dc
tiennvuit <tiennvuit@gmail.com>
2020-08-14 17:49:50 +0700

    add

--- algorithms/shannon_fano.py
+++ algorithms/shannon_fano.py
@@ -2,43 +2,55 @@ import argparse
 import os
 import time
 from collections import Counter
+from .base import Base
 
+class ShanonFano(Base):
+    def __init__(self):
+        self.name = "Shanon Fano algorithm"
 
-def shanon_FanoCompression(input_string: str):
+    def compress(self, input: str):
 
-    print("[INFO] The string need encoded is \n\t{}".format(input_string))
+        input_string = input
 
-    # Count the frequency of the symbols in input_string
-    print("[INFO] Counting the frequency of symbols in string ...")
-    frequency_count = Counter(sorted(input_string))
-    del frequency_count['\n']
-    time.sleep(1)
+        print("[INFO] The string need encoded is \n\t{}".format(input_string))
 
-    # Sort the frequency_count ascending.
-    sorted_counts = {k: v for k, v in sorted(frequency_count.items(), key=lambda item: item[1])}
-
-
-
-def shanon_FanoDecompression(encoded_string):
-    res_string = ""
-
-    print("[INFO] The string need decoded is\n{}".format(encoded_string))
-    count_characters = encoded_string.split("\n")
-
-    print("[INFO] Decoding ...")
-    for character in count_characters:
-        if character == "":
-            continue
-        (c, count) = character.split(":")
-
-        res_string += c * int(count)
-
-        print("\tDecoded ...{}".format(c * int(count)))
+        # Count the frequency of the symbols in input_string
+        print("[INFO] Counting the frequency of symbols in string ...")
+        frequency_count = Counter(sorted(input_string))
+        del frequency_count['\n']
         time.sleep(1)
 
-    print("[INFO] The decoded string is {}".format(res_string))
-    return res_string
+        # Sort the frequency_count ascending.
+        sorted_counts = {k: v for k, v in sorted(frequency_count.items(), key=lambda item: item[1])}
 
+    def decompress(self, encoded):
+
+        encoded_string = encoded
+
+        res_string = ""
+
+        print("[INFO] The string need decoded is\n{}".format(encoded_string))
+        count_characters = encoded_string.split("\n")
+
+        print("[INFO] Decoding ...")
+        for character in count_characters:
+            if character == "":
+                continue
+            (c, count) = character.split(":")
+
+            res_string += c * int(count)
+
+            print("\tDecoded ...{}".format(c * int(count)))
+            time.sleep(1)
+
+        print("[INFO] The decoded string is {}".format(res_string))
+        return res_string
+
+    def calculate_compression_ratio(self, input, encoded, dictionary):
+        b0 = len(input)*8
+        b1 = len(encoded) + len(dictionary)*40
+        return b0 / b1
+        
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='The Shannon-Fano algorithms')
@@ -50,7 +62,7 @@ def get_arguments():
     parser.add_argument('--output', '-o', default='./output/output_shanon-fano.txt',
                         help='The output file path')
     return vars(parser.parse_args())
-    
+
 
 def main(args):
     # Check the input path is exis whether or not
